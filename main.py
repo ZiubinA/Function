da9a4321a68776deb6ea887467e52b04b32362b8
MrKingPro <49118782+ZiubinA@users.noreply.github.com>
2021-06-19 18:22:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def say_hello():
+  print('Hello World!')
+
+say_hello()
+
+def say_hello(name):
+  print(f'Hello    {name}!')
+
+say_hello('ARSENII')  
+
+def say_hello(name='World'):
+  print(f'Hello {name}!')
+say_hello()
+say_hello('ARSENII')  
